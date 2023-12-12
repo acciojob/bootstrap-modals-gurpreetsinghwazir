6c96c0d9a601a63644a494460231f0f2dfcaaf2e
gurpreetsinghwazir <143097451+gurpreetsinghwazir@users.noreply.github.com>
2023-12-12 15:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+var myModal = document.getElementById('newton-modal');
+var myInput = document.getElementById('open-modal');
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus();
+});
